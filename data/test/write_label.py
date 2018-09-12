12321266c6a9afdba09ca672fd2b962f5eef725d
Liulong Ma <marooncn@163.com>
2018-09-12 09:51:19 +0800

    Add files via upload

--- data/test/write_label.py
+++ data/test/write_label.py
@@ -3,7 +3,7 @@
 
 f = open("label.csv", "w")
 for i in range(40):
-    f.write('./normal/' + str(i+1)+'.jpg'+',1\n')
+    f.write('./data/test/normal/' + str(i+1)+'.jpg'+',1\n')
 for i in range(40):
-    f.write('./defect/' + str(i+1)+'.jpg'+',0\n')
+    f.write('./data/test/defect/' + str(i+1)+'.jpg'+',0\n')
 f.close()
