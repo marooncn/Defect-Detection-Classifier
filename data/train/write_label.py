b3a6ec5aa7c8f0c19b8b7cc30d0aee8d1ba514de
Liulong Ma <marooncn@163.com>
2018-09-12 09:52:10 +0800

    Add files via upload

--- data/train/write_label.py
+++ data/train/write_label.py
@@ -3,7 +3,7 @@
 
 f = open("label.csv", "w")
 for i in range(160):
-    f.write('./normal/' + str(i+1)+'.jpg'+',1\n')
+    f.write('./data/train/normal/' + str(i+1)+'.jpg'+',1\n')
 for i in range(160):
-    f.write('./defect/' + str(i+1)+'.jpg'+',0\n')
+    f.write('./data/train/defect/' + str(i+1)+'.jpg'+',0\n')
 f.close()
